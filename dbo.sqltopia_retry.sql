2bf2efbb156af855363ec09700b79a9ff9ae5734
Peter Larsson <mvp@sqltopia.com>
2020-12-04 19:37:15 +0100

    Update dbo.sqltopia_retry.sql

--- dbo.sqltopia_retry.sql
+++ dbo.sqltopia_retry.sql
@@ -7,8 +7,8 @@ ALTER PROCEDURE dbo.sqltopia_retry
         @max_retry_count TINYINT = 3
 )
 /*
-        sqltopia_retry v1.7.5 (2020-12-03)
-        (C) 2009-2020, Peter Larsson
+        sqltopia_retry v2.0.0 (2021-01-01)
+        (C) 2009-2021, Peter Larsson
 */
 AS
 
