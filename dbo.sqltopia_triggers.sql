49376578d218ff8b679c160b1ec7c4a6e0fc422a
Peter Larsson <mvp@sqltopia.com>
2020-12-04 11:50:56 +0100

    Changed sqltopia_triggers to allow for multiple choice

--- dbo.sqltopia_triggers.sql
+++ dbo.sqltopia_triggers.sql
@@ -5,7 +5,8 @@ ALTER FUNCTION dbo.sqltopia_triggers
 (
         @check_if_object_exist BIT = 1,
         @schema_name SYSNAME = NULL,
-        @table_name SYSNAME = NULL
+        @table_name SYSNAME = NULL,
+        @trigger_name SYSNAME = NULL
 )
 /*
         sqltopia_triggers v2.0.0 (2021-01-01)
@@ -13,39 +14,41 @@ ALTER FUNCTION dbo.sqltopia_triggers
 */
 RETURNS TABLE
 AS
-RETURN  WITH cteTriggers(schema_id, schema_name, table_id, table_name, trigger_name, definition, precheck)
+RETURN  WITH cteTriggers(schema_id, schema_name, table_id, table_name, trigger_name, trigger_definition, precheck)
         AS (
                 SELECT          sch.schema_id,
                                 sch.schema_name,
                                 tbl.table_id,
                                 tbl.table_name,
                                 trg.trigger_name,
-                                sqm.definition COLLATE DATABASE_DEFAULT AS definition,
+                                sqm.trigger_definition,
                                 CONCAT(N'EXISTS (SELECT * FROM sys.triggers WHERE name COLLATE DATABASE_DEFAULT = N', QUOTENAME(trg.trigger_name, N''''), N')') AS precheck
                 FROM            (
                                         SELECT  parent_id AS table_id,
                                                 object_id AS trigger_id,
                                                 name COLLATE DATABASE_DEFAULT AS trigger_name
                                         FROM    sys.triggers
-                                        WHERE   parent_id = OBJECT_ID(QUOTENAME(@schema_name) + N'.' + QUOTENAME(@table_name), 'U')
-                                                OR @schema_name IS NULL AND @table_name IS NULL
+                                        WHERE   name COLLATE DATABASE_DEFAULT = @trigger_name AND @trigger_name IS NOT NULL
+                                                OR @trigger_name IS NULL
                                 ) AS trg
                 INNER JOIN      (
                                         SELECT  schema_id,
                                                 object_id AS table_id,
                                                 name COLLATE DATABASE_DEFAULT AS table_name
                                         FROM    sys.tables
-                                        WHERE   object_id = OBJECT_ID(QUOTENAME(@schema_name) + N'.' + QUOTENAME(@table_name), 'U')
-                                                OR @schema_name IS NULL AND @table_name IS NULL
+                                        WHERE   name COLLATE DATABASE_DEFAULT = @table_name AND @table_name IS NOT NULL
+                                                OR @table_name IS NULL
                                 ) AS tbl ON tbl.table_id = trg.table_id
                 INNER JOIN      (
                                         SELECT  schema_id,
                                                 name COLLATE DATABASE_DEFAULT AS schema_name
                                         FROM    sys.schemas
+                                        WHERE   name COLLATE DATABASE_DEFAULT = @schema_name AND @schema_name IS NOT NULL
+                                                OR @schema_name IS NULL
                                 ) AS sch ON sch.schema_id = tbl.schema_id
                 INNER JOIN      (
                                         SELECT  object_id AS trigger_id,
-                                                definition COLLATE DATABASE_DEFAULT AS definition
+                                                definition COLLATE DATABASE_DEFAULT AS trigger_definition
                                         FROM    sys.sql_modules
                                 ) AS sqm ON sqm.trigger_id = trg.trigger_id
         )
@@ -69,7 +72,7 @@ RETURN  WITH cteTriggers(schema_id, schema_name, table_id, table_name, trigger_n
                                         ),
                                         (
                                                 N'create',
-                                                CONCAT(cte.definition, N';')
+                                                CONCAT(cte.trigger_definition, N';')
                                         ),
                                         (
                                                 N'disable',
