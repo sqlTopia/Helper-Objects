4586b71d15a90dd51743dca794059447c70d66f1
Peter Larsson <mvp@sqltopia.com>
2020-12-04 15:51:22 +0100

    Update dbo.sqltopia_datatype_rules.sql

--- dbo.sqltopia_datatype_rules.sql
+++ dbo.sqltopia_datatype_rules.sql
@@ -6,29 +6,50 @@ ALTER FUNCTION dbo.sqltopia_datatype_rules
         @check_if_object_exist BIT = 1
 )
 /*
-        sqltopia_datatype_rules v1.7.5 (2020-12-03)
-        (C) 2009-2020, Peter Larsson
+        sqltopia_datatype_rules v2.0.0 (2021-01-01)
+        (C) 2009-2021, Peter Larsson
 */
 RETURNS TABLE
 AS
-RETURN  WITH cteRules(schema_id, schema_name, table_id, table_name, column_id, column_name, rule_name, definition, precheck)
+RETURN  WITH cteRules(schema_id, schema_name, table_id, table_name, column_id, column_name, rule_id, rule_name, rule_definition, precheck)
         AS (
                 SELECT          sch.schema_id,
-                                sch.name COLLATE DATABASE_DEFAULT AS schema_name,
-                                tbl.object_id AS table_id,
-                                tbl.name COLLATE DATABASE_DEFAULT AS table_name,
+                                sch.schema_name,
+                                tbl.table_id,
+                                tbl.table_name,
                                 col.column_id,
-                                col.name COLLATE DATABASE_DEFAULT AS column_name,
-                                obj.name COLLATE DATABASE_DEFAULT AS rule_name,
-                                sqm.definition COLLATE DATABASE_DEFAULT AS definition,
-                                CONCAT(N'EXISTS (SELECT * FROM sys.columns AS cc INNER JOIN sys.tables AS tbl ON tbl.object_id = cc.object_id AND tbl.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(tbl.name COLLATE DATABASE_DEFAULT, N''''), N' INNER JOIN sys.schemas AS sch ON sch.schema_id = tbl.schema_id AND sch.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(sch.name COLLATE DATABASE_DEFAULT, N''''), N' WHERE cc.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(col.name COLLATE DATABASE_DEFAULT, N''''), N' AND cc.rule_object_id <> 0)') AS precheck
-                FROM            sys.columns AS col
-                INNER JOIN      sys.tables AS tbl ON tbl.object_id = col.object_id
-                INNER JOIN      sys.schemas AS sch ON sch.schema_id = tbl.schema_id
-                INNER JOIN      sys.objects AS obj ON obj.object_id = col.rule_object_id
-                                        AND obj.type COLLATE DATABASE_DEFAULT = N'R'
-                INNER JOIN      sys.sql_modules AS sqm ON sqm.object_id = col.rule_object_id
-                WHERE           col.rule_object_id <> 0
+                                col.column_name,
+                                r.rule_id,
+                                r.rule_name,
+                                r.rule_definition,
+                                CONCAT(N'EXISTS (SELECT * FROM sys.columns AS cc INNER JOIN sys.tables AS tbl ON tbl.object_id = cc.object_id AND tbl.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(tbl.table_name, N''''), N' INNER JOIN sys.schemas AS sch ON sch.schema_id = tbl.schema_id AND sch.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(sch.schema_name, N''''), N' WHERE cc.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(col.column_name, N''''), N' AND cc.rule_object_id <> 0)') AS precheck
+                FROM            (
+                                        SELECT  object_id AS table_id,
+                                                column_id,
+                                                name COLLATE DATABASE_DEFAULT AS column_name,
+                                                rule_object_id AS rule_id
+                                        FROM    sys.columns
+                                ) AS col
+                INNER JOIN      (
+                                        SELECT  schema_id,
+                                                object_id AS table_id,
+                                                name COLLATE DATABASE_DEFAULT AS table_name
+                                        FROM    sys.tables
+                                ) AS tbl ON tbl.table_id = col.table_id
+                INNER JOIN      (
+                                        SELECT  schema_id,
+                                                name COLLATE DATABASE_DEFAULT AS schema_name
+                                        FROM    sys.schemas
+                                ) AS sch ON sch.schema_id = tbl.schema_id
+                INNER JOIN      (
+                                        SELECT          obj.object_id AS rule_id,
+                                                        obj.name COLLATE DATABASE_DEFAULT AS rule_name,
+                                                        sqm.definition COLLATE DATABASE_DEFAULT AS rule_definition
+                                        FROM            sys.objects AS obj
+                                        INNER JOIN      sys.sql_modules AS sqm ON sqm.object_id = obj.object_id
+                                        WHERE           obj.type COLLATE DATABASE_DEFAULT = 'R'
+                                                        AND obj.object_id <> 0
+                                ) AS r ON r.rule_id = col.rule_id
         )
         SELECT          cte.schema_id, 
                         cte.schema_name, 
@@ -36,6 +57,8 @@ RETURN  WITH cteRules(schema_id, schema_name, table_id, table_name, column_id, c
                         cte.table_name, 
                         cte.column_id, 
                         cte.column_name,
+                        cte.rule_id,
+                        cte.rule_name,
                         CAST(act.query_action AS NVARCHAR(8)) AS query_action,
                         CASE
                                 WHEN @check_if_object_exist = 1 AND act.query_action = N'unbind' THEN CONCAT(N'IF ', cte.precheck, N' ', act.query_text)
@@ -60,7 +83,7 @@ RETURN  WITH cteRules(schema_id, schema_name, table_id, table_name, column_id, c
                                         ),
                                         (
                                                 N'create',
-                                                CONCAT(cte.definition, N';')
+                                                CONCAT(cte.rule_definition, N';')
                                         )
                         ) AS act(query_action, query_text);
 GO
