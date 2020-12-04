2221e356e2572422aca9f5711d70acd3df01ade8
Peter Larsson <mvp@sqltopia.com>
2020-12-04 15:13:10 +0100

    Update dbo.sqltopia_default_constraints.sql

--- dbo.sqltopia_default_constraints.sql
+++ dbo.sqltopia_default_constraints.sql
@@ -6,35 +6,58 @@ ALTER FUNCTION dbo.sqltopia_default_constraints
         @check_if_object_exist BIT = 1
 )
 /*
-        sqltopia_default_constraints v1.7.5 (2020-12-03)
-        (C) 2009-2020, Peter Larsson
+        sqltopia_default_constraints v2.0.0 (2021-01-01)
+        (C) 2009-2021, Peter Larsson
 */
 RETURNS TABLE
 AS
-RETURN  WITH cteDefaultConstraints(default_constraint_name, schema_id, schema_name, table_id, table_name, column_id, column_name, definition, precheck)
+RETURN  WITH cteDefaultConstraints(schema_id, schema_name, table_id, table_name, column_id, column_name, default_constraint_id, default_constraint_name, defafult_definition, precheck)
         AS (
-                SELECT          dc.name COLLATE DATABASE_DEFAULT AS default_constraint_name,
-                                sch.schema_id,
-                                sch.name COLLATE DATABASE_DEFAULT AS schema_name,
-                                tbl.object_id AS table_id,
-                                tbl.name COLLATE DATABASE_DEFAULT AS table_name,
+                SELECT          sch.schema_id,
+                                sch.schema_name,
+                                tbl.table_id,
+                                tbl.table_name,
                                 col.column_id,
-                                col.name COLLATE DATABASE_DEFAULT AS column_name,
-                                dc.definition COLLATE DATABASE_DEFAULT AS definition,
-                                CONCAT(N'EXISTS (SELECT * FROM sys.default_constraints WHERE name COLLATE DATABASE_DEFAULT = N', QUOTENAME(dc.name COLLATE DATABASE_DEFAULT, N''''), N')') AS precheck
-                FROM            sys.default_constraints AS dc
-                INNER JOIN      sys.tables AS tbl ON tbl.object_id = dc.parent_object_id
-                INNER JOIN      sys.schemas AS sch ON sch.schema_id = tbl.schema_id
-                INNER JOIN      sys.columns AS col ON col.object_id = dc.parent_object_id
-                WHERE           col.column_id = dc.parent_column_id 
+                                col.column_name,
+                                dc.default_constraint_id,
+                                dc.default_constraint_name,
+                                dc.default_definition,
+                                CONCAT(N'EXISTS (SELECT * FROM sys.default_constraints WHERE name COLLATE DATABASE_DEFAULT = N', QUOTENAME(dc.default_constraint_name, N''''), N')') AS precheck
+                FROM            (
+                                        SELECT  parent_object_id AS table_id,
+                                                object_id AS default_constraint_id,
+                                                name COLLATE DATABASE_DEFAULT AS default_constraint_name,
+                                                parent_column_id AS column_id,
+                                                definition COLLATE DATABASE_DEFAULT AS default_definition
+                                        FROM    sys.default_constraints
+                                ) AS dc
+                INNER JOIN      (
+                                        SELECT  schema_id,
+                                                object_id AS table_id,
+                                                name COLLATE DATABASE_DEFAULT AS table_name
+                                        FROM    sys.tables
+                                ) AS tbl ON tbl.table_id = dc.table_id
+                INNER JOIN      (
+                                        SELECT  schema_id,
+                                                name COLLATE DATABASE_DEFAULT AS schema_name
+                                        FROM    sys.schemas
+                                ) AS sch ON sch.schema_id = tbl.schema_id
+                INNER JOIN      (
+                                        SELECT  object_id AS table_id,
+                                                column_id,
+                                                name COLLATE DATABASE_DEFAULT AS column_name
+                                        FROM    sys.columns
+                                ) AS col ON col.table_id = dc.table_id
+                                        AND col.column_id = dc.column_id 
         )
-        SELECT          cte.default_constraint_name, 
-                        cte.schema_id, 
+        SELECT          cte.schema_id, 
                         cte.schema_name, 
                         cte.table_id, 
                         cte.table_name, 
                         cte.column_id, 
                         cte.column_name,
+                        cte.default_constraint_id, 
+                        cte.default_constraint_name, 
                         CAST(act.query_action AS NVARCHAR(8)) AS query_action,
                         CASE
                                 WHEN @check_if_object_exist = 1 AND act.query_action = N'drop' THEN CONCAT(N'IF ', cte.precheck, N' ', act.query_text)
