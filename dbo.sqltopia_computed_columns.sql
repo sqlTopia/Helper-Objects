eb0bb0128bebcc57869ff0123f4972ce2d2718dc
Peter Larsson <mvp@sqltopia.com>
2020-12-04 15:32:42 +0100

    Update dbo.sqltopia_computed_columns.sql

--- dbo.sqltopia_computed_columns.sql
+++ dbo.sqltopia_computed_columns.sql
@@ -6,30 +6,48 @@ ALTER FUNCTION dbo.sqltopia_computed_columns
         @check_if_object_exist BIT = 1
 )
 /*
-        sqltopia_computed_columns v1.7.5 (2020-12-03)
-        (C) 2009-2020, Peter Larsson
+        sqltopia_computed_columns v2.0.0 (2021-01-01)
+        (C) 2009-2021, Peter Larsson
 */
 RETURNS TABLE
 AS
-RETURN  WITH cteComputedColumns(schema_id, schema_name, table_id, table_name, column_id, column_name, computed_column_name, definition, is_persisted, precheck)
+RETURN  WITH cteComputedColumns(schema_id, schema_name, table_id, table_name, column_id, column_name, computed_column_name, computed_definition, is_persisted, precheck)
         AS (
                 SELECT          sch.schema_id,
-                                sch.name COLLATE DATABASE_DEFAULT AS schema_name,
-                                tbl.object_id AS table_id,
-                                tbl.name COLLATE DATABASE_DEFAULT AS table_name,
+                                sch.schema_name,
+                                tbl.table_id,
+                                tbl.table_name,
                                 col.column_id,
-                                col.name COLLATE DATABASE_DEFAULT AS column_name,
-                                c.name COLLATE DATABASE_DEFAULT AS computed_column_name,
-                                cc.definition COLLATE DATABASE_DEFAULT AS definition,
+                                col.column_name,
+                                cc.computed_column_name,
+                                cc.computed_definition,
                                 cc.is_persisted,
-                                CONCAT(N'EXISTS (SELECT * FROM sys.computed_columns AS cc INNER JOIN sys.tables AS tbl ON tbl.object_id = cc.object_id AND tbl.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(tbl.name COLLATE DATABASE_DEFAULT, N''''), N' INNER JOIN sys.schemas AS sch ON sch.schema_id = tbl.schema_id AND sch.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(sch.name COLLATE DATABASE_DEFAULT, N''''), N' WHERE cc.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(c.name COLLATE DATABASE_DEFAULT, N''''), N')') AS precheck
-                FROM            sys.schemas AS sch
-                INNER JOIN      sys.tables AS tbl ON tbl.schema_id = sch.schema_id
-                INNER JOIN      sys.columns AS col ON col.object_id = tbl.object_id
-                INNER JOIN      sys.computed_columns AS cc ON cc.object_id = col.object_id
-                                        AND CHARINDEX(QUOTENAME(col.name), cc.definition) >= 1
-                INNER JOIN      sys.columns AS c ON c.object_id = cc.object_id
-                                        AND c.column_id = cc.column_id
+                                CONCAT(N'EXISTS (SELECT * FROM sys.computed_columns AS cc INNER JOIN sys.tables AS tbl ON tbl.object_id = cc.object_id AND tbl.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(tbl.table_name, N''''), N' INNER JOIN sys.schemas AS sch ON sch.schema_id = tbl.schema_id AND sch.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(sch.schema_name, N''''), N' WHERE cc.name COLLATE DATABASE_DEFAULT = N', QUOTENAME(cc.computed_column_name, N''''), N')') AS precheck
+                FROM            (
+                                        SELECT  schema_id,
+                                                name COLLATE DATABASE_DEFAULT AS schema_name
+                                        FROM    sys.schemas
+                                ) AS sch
+                INNER JOIN      (
+                                        SELECT  schema_id,
+                                                object_id AS table_id,
+                                                name COLLATE DATABASE_DEFAULT AS table_name
+                                        FROM    sys.tables
+                                ) AS tbl ON tbl.schema_id = sch.schema_id
+                INNER JOIN      (
+                                        SELECT  object_id AS table_id,
+                                                column_id,
+                                                name COLLATE DATABASE_DEFAULT AS column_name
+                                        FROM    sys.columns
+                                ) AS col ON col.table_id = tbl.table_id
+                INNER JOIN      (
+                                        SELECT  object_id AS table_id,
+                                                name COLLATE DATABASE_DEFAULT AS computed_column_name,
+                                                definition COLLATE DATABASE_DEFAULT AS computed_definition,
+                                                is_persisted
+                                        FROM    sys.computed_columns
+                                ) AS cc ON cc.table_id = col.table_id
+                                        AND CHARINDEX(QUOTENAME(col.column_name), cc.computed_definition) >= 1
         )
         SELECT          cte.schema_id, 
                         cte.schema_name, 
@@ -51,7 +69,7 @@ RETURN  WITH cteComputedColumns(schema_id, schema_name, table_id, table_name, co
                                         ),
                                         (
                                                 N'create',
-                                                CONCAT(N'ALTER TABLE ', QUOTENAME(cte.schema_name), N'.', QUOTENAME(cte.table_name), N' ADD ', QUOTENAME(cte.computed_column_name), N' AS ', cte.definition, CASE WHEN cte.is_persisted = 1 THEN N' PERSISTED;' ELSE N';' END)
+                                                CONCAT(N'ALTER TABLE ', QUOTENAME(cte.schema_name), N'.', QUOTENAME(cte.table_name), N' ADD ', QUOTENAME(cte.computed_column_name), N' AS ', cte.computed_definition, CASE WHEN cte.is_persisted = 1 THEN N' PERSISTED;' ELSE N';' END)
                                         )
                         ) AS act(query_action, query_text);
 GO
